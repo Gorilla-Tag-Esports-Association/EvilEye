d2a88293086b5d3d06ca6af3941bc5537655f046
Slav-XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2025-06-17 18:18:44 -0400

    Dll injection prompt for syonic

--- HOST/src/dll_injection.c
+++ HOST/src/dll_injection.c
@@ -1,1 +1,6 @@
 #include <stdio.h>
+
+
+void dll_inject(const char *dll_path, const char *process_name){
+    
+}
--- HOST/src/headers/dll_injection.h
+++ HOST/src/headers/dll_injection.h
@@ -1,4 +1,6 @@
 #ifndef DLL_INJECTION_H
 #define DLL_INJECTION_H
 
+void dll_inject(const char *dll_path, const char *process_name);
+
 #endif
