d48350a27d2bf3d6163ea0193fd652bd2dfb2f09
Slav-XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2025-06-29 18:25:56 -0400

    Fixing up dll injection // New hashes

--- HOST/src/headers/secrets.h
+++ HOST/src/headers/secrets.h
@@ -5,6 +5,6 @@
 #define PIPE_NAME "\\\\.\\pipe\\"
 #define PORT "443"
 #define HOST "discord.com"
-#define URL "/api/webhooks/1389008406119186442/JAxLM4WL1aD2F-Si3A5ym8SW0iMv-AKx5hhfFijIoEhv03DzbqqLVpgtM9lmf4fvfoSQ"
+#define URL ""
 
 #endif
